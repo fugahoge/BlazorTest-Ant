8236792cafe0a7037acef7a2413b76c7014082f9
fuga hoge <fugahoge@gmail.com>
2024-10-15 13:26:23 +0900

    add validation

--- BlazorTest_Ant/Program.cs
+++ BlazorTest_Ant/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Negotiate;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
 using BlazorTest_Ant.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +10,40 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// 認証サービスを追加(Cookie認証)
+builder.Services
+  .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+  .AddCookie
+  (
+    options =>
+    {
+      // ログインページのURL
+      options.LoginPath = "/login";
+    }
+  );
+
+// 認証サービスを追加(Windwos認証)
+//builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme).AddNegotiate();
+
+// 認可サービスを追加
+builder.Services.AddAuthorization(options =>
+{
+  // AllowAnonymous属性が指定されていない全ての画面でユーザー認証を要求する
+  options.FallbackPolicy = new AuthorizationPolicyBuilder()
+    .RequireAuthenticatedUser()
+    .Build();
+
+  // デフォルトのフォールバックポリシーを使う場合
+  //options.FallbackPolicy = options.DefaultPolicy;
+});
+
+// Blazor用の認証情報を提供するためのコンポーネント
+builder.Services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
+
+// 画面で情報を取得するための設定
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddHttpContextAccessor();
 
 // 
 builder.Services.AddAntDesign();
@@ -19,6 +58,10 @@ if (!app.Environment.IsDevelopment())
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+// アプリに認証と認可を追加
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
